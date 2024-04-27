b2e6882bd060e085e81e029938558a2b1a8f51a5
MoonesMezher <moonesmezher050@gmail.com>
2024-04-28 00:02:21 +0300

    header handling

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -2,9 +2,13 @@ import { PiPhone } from "react-icons/pi"
 import { NavLink } from "react-router-dom"
 import './Header.css'
 import { useState } from "react"
+import { MdMenu } from "react-icons/md"
+import { CgClose } from "react-icons/cg"
 
 const Header = () => {
     let [changeColor, setChangeColor] = useState<boolean>(false);
+
+    let [showMenu, setShowMenu] = useState<boolean>(false);
 
     const links = [
         {
@@ -36,8 +40,10 @@ const Header = () => {
     };
 
     return (
-        <header className={`mm-header main-container ${changeColor ? 'bg-green': ''}`}>
-            <div>
+        <header className={`mm-header main-container ${changeColor ? 'changed-header': ''} ${showMenu && 'show-menu' }`}>
+            {!showMenu ?<MdMenu className="menu" onClick={() => setShowMenu(true)}/>
+                :<CgClose className="menu" onClick={() => setShowMenu(false)}/>}
+            <div className="left-side">
                 <span className="logo font-5">X5</span>
                 <ul>
                     {links.map((e, i) => <li key={i}><NavLink to={e.url} className={'active-link font--1'}>{e.title}</NavLink></li>)}
