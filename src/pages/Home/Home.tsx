b2f330894203c650fe19f0482544ef03024ae954
MoonesMezher <moonesmezher050@gmail.com>
2024-04-29 19:16:52 +0300

    last edits

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import Companies from '../../components/home/Companies/Companies'
 import Hero from '../../components/home/Hero/Hero'
 import MyProject from '../../components/MyProject/MyProject'
+import Services from '../../components/Services/Services'
 import './Home.css'
 
 const Home = () => {
@@ -8,7 +9,7 @@ const Home = () => {
     return (
         <section className="home page">
             <Hero/>
-            {/* <Ser/> */}
+            <Services/>
             <Companies/>            
             <MyProject/>
         </section>
