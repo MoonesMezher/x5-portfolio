173d966cc91b6918a1c002d580438edd63950fd6
EsmaelAbd <esmaelabd51@gmail.com>
2024-04-29 14:50:22 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import './assets/css/colors.css'
-import './assets/css/fonts.css'
-import './assets/css/layout.css'
-import './App.css'
-import Home from './pages/Home/Home';
-import Header from './components/shared/Header/Header';
-import Footer from './components/shared/Footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./assets/css/colors.css";
+import "./assets/css/fonts.css";
+import "./assets/css/layout.css";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import Header from "./components/shared/Header/Header";
+import Footer from "./components/shared/Footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Services from "./components/Services/Services";
 
 function App() {
   return (
-    <main>
+    <div>
+      <main>
         <BrowserRouter>
-          <Header/>
+          <Header />
           <Routes>
-            <Route path='/' element={<Home/>}/>
+            <Route path="/" element={<Home />} />
           </Routes>
-          <Footer/>
+          <Footer />
         </BrowserRouter>
-    </main>
-  )
+      </main>
+
+      <Services />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Services/Services.tsx
@@ -0,0 +1,14 @@
+import Container from './Containers/Container'
+import './Services.css'
+import Title from './title/Title'
+
+const Services = () => {
+  return (
+    <div className='services'>
+        <Title/>
+        <Container/>
+    </div>
+  )
+}
+
+export default Services
